fb14e9a821bf4c14b3579b336191cd8666a2650a
caduxl007 <eduardo2020dev@gmail.com>
2022-12-07 23:00:00 -0300

    feat: add component Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro reprehenderit est veniam? Rem recusandae sint perspiciatis debitis corrupti. Deserunt neque exercitationem illo voluptatum laudantium culpa suscipit accusantium blanditiis voluptas numquam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
